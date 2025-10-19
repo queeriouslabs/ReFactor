56059718f925c92a82f1fe1a2c9ea013281cfac6
Beka Valentine <beka@gothdyke.mom>
2025-10-18 17:36:36 -0700

    Adds more changes

--- loom/wip/jacquard_module/variant_7_dual_cylinder_with_bearings/python/main.py
+++ loom/wip/jacquard_module/variant_7_dual_cylinder_with_bearings/python/main.py
@@ -24,37 +24,39 @@ def multistep(count, direction):
 
 RELEASE_CYLINDER_POSITION_COUNT = 21
 TOTAL_STEPS = 200
+ADVANCE_FRAC = 1/4
+ADVANCE_DELTA = 4
 
 def advance_release_cylinder():
-  release_steps = (1/6) * TOTAL_STEPS
-  multistep(count = release_steps, direction = 'ccw')
+  release_steps = ADVANCE_FRAC * TOTAL_STEPS
+  multistep(count = release_steps+ADVANCE_DELTA, direction = 'ccw')
   multistep(count = release_steps, direction = 'cw')
 
 def release_current_heddle():
   multistep(count = 30, direction = 'cw')
   multistep(count = 30, direction = 'ccw')
 
-def reset_heddles():
-  multistep(count = 203, direction = 'cw')
+def reset_heddles(delta = 0):
+  multistep(count = TOTAL_STEPS + delta, direction = 'cw')
 
 def set_heddles(bits):
   for _, bit in enumerate(bits):
-    advance_release_cylinder() # move to next heddle
     if bit == 0:
       release_current_heddle()
+    advance_release_cylinder() # move to next heddle
 
-  advance_release_cylinder() # move to reset position
+  # advance_release_cylinder() # move to reset position
 
 def test_release_cylinder_full_cycle():
   for _ in range(RELEASE_CYLINDER_POSITION_COUNT):
     advance_release_cylinder()
 
 def test_heddle_sequence():
-  for _ in range(1):
+  while True:
     pattern = [1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0]
     set_heddles(pattern)
-    time.sleep(0.5)
-    reset_heddles()
+    input()
+    # reset_heddles()
     set_heddles([ 1-b for b in pattern ])
-    time.sleep(0.5)
-    reset_heddles()
+    input()
+    # reset_heddles()
